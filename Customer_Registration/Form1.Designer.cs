d4feb9b2c5e7f146f7a4ce9cfb4b8d0a7578036d
RavinduThilina222 <127467815+RavinduThilina222@users.noreply.github.com>
2023-03-20 10:18:55 +0530

    Add project files.

---
+++ Customer_Registration/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace Customer_Registration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_cid = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt_phoneNo = new System.Windows.Forms.TextBox();
+            this.txt_Address = new System.Windows.Forms.TextBox();
+            this.txt_nicNumber = new System.Windows.Forms.TextBox();
+            this.txt_Email = new System.Windows.Forms.TextBox();
+            this.txt_lname = new System.Windows.Forms.TextBox();
+            this.txt_fName = new System.Windows.Forms.TextBox();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.btn_register = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_cid
+            // 
+            this.txt_cid.Location = new System.Drawing.Point(311, 76);
+            this.txt_cid.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_cid.Name = "txt_cid";
+            this.txt_cid.Size = new System.Drawing.Size(210, 22);
+            this.txt_cid.TabIndex = 31;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(107, 82);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(85, 17);
+            this.label7.TabIndex = 30;
+            this.label7.Text = "Customer ID";
+            // 
+            // txt_phoneNo
+            // 
+            this.txt_phoneNo.Location = new System.Drawing.Point(311, 254);
+            this.txt_phoneNo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_phoneNo.Name = "txt_phoneNo";
+            this.txt_phoneNo.Size = new System.Drawing.Size(210, 22);
+            this.txt_phoneNo.TabIndex = 29;
+            // 
+            // txt_Address
+            // 
+            this.txt_Address.Location = new System.Drawing.Point(311, 300);
+            this.txt_Address.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_Address.Name = "txt_Address";
+            this.txt_Address.Size = new System.Drawing.Size(210, 22);
+            this.txt_Address.TabIndex = 28;
+            // 
+            // txt_nicNumber
+            // 
+            this.txt_nicNumber.Location = new System.Drawing.Point(311, 352);
+            this.txt_nicNumber.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_nicNumber.Name = "txt_nicNumber";
+            this.txt_nicNumber.Size = new System.Drawing.Size(210, 22);
+            this.txt_nicNumber.TabIndex = 27;
+            // 
+            // txt_Email
+            // 
+            this.txt_Email.Location = new System.Drawing.Point(311, 199);
+            this.txt_Email.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_Email.Name = "txt_Email";
+            this.txt_Email.Size = new System.Drawing.Size(210, 22);
+            this.txt_Email.TabIndex = 26;
+            // 
+            // txt_lname
+            // 
+            this.txt_lname.Location = new System.Drawing.Point(311, 156);
+            this.txt_lname.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_lname.Name = "txt_lname";
+            this.txt_lname.Size = new System.Drawing.Size(210, 22);
+            this.txt_lname.TabIndex = 25;
+            // 
+            // txt_fName
+            // 
+            this.txt_fName.Location = new System.Drawing.Point(311, 116);
+            this.txt_fName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_fName.Name = "txt_fName";
+            this.txt_fName.Size = new System.Drawing.Size(210, 22);
+            this.txt_fName.TabIndex = 24;
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(612, 218);
+            this.btn_clear.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(86, 28);
+            this.btn_clear.TabIndex = 23;
+            this.btn_clear.Text = "Clear";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.Btn_clear_Click);
+            // 
+            // btn_register
+            // 
+            this.btn_register.Location = new System.Drawing.Point(612, 161);
+            this.btn_register.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_register.Name = "btn_register";
+            this.btn_register.Size = new System.Drawing.Size(86, 28);
+            this.btn_register.TabIndex = 22;
+            this.btn_register.Text = "Register";
+            this.btn_register.UseVisualStyleBackColor = true;
+            this.btn_register.Click += new System.EventHandler(this.Btn_register_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(104, 259);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(103, 17);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Phone Number";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(104, 305);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 17);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Address";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(104, 358);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 17);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "NIC";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(104, 205);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 17);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "email";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(102, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 17);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Last Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(104, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 17);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "First name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_cid);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txt_phoneNo);
+            this.Controls.Add(this.txt_Address);
+            this.Controls.Add(this.txt_nicNumber);
+            this.Controls.Add(this.txt_Email);
+            this.Controls.Add(this.txt_lname);
+            this.Controls.Add(this.txt_fName);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_register);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_cid;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txt_phoneNo;
+        private System.Windows.Forms.TextBox txt_Address;
+        private System.Windows.Forms.TextBox txt_nicNumber;
+        private System.Windows.Forms.TextBox txt_Email;
+        private System.Windows.Forms.TextBox txt_lname;
+        private System.Windows.Forms.TextBox txt_fName;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.Button btn_register;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Customer_Registration/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Customer_Registration
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con;
+        SqlCommand cmd;
+
+
+        private void Btn_register_Click(object sender, EventArgs e)
+        {
+            string tp;
+            String nic;
+
+            tp = Convert.ToString(txt_phoneNo.Text);
+            nic = Convert.ToString(txt_nicNumber.Text);
+
+            try
+            {
+
+                SqlConnection con = new SqlConnection("Data Source=DESKTOP-OC4GVNP;Initial Catalog=MotorSpaLK;Integrated Security=True");
+                con.Open();
+
+                cmd = new SqlCommand("Insert into Customer values ('" + txt_cid.Text + "','" + txt_fName.Text + "','" + txt_lname.Text + "','" + txt_Email.Text + "','" + txt_phoneNo.Text + "','" + txt_Address.Text + "','" + txt_nicNumber.Text + "')", con);
+
+
+
+                if (txt_fName.Text.Any(char.IsDigit) || string.IsNullOrEmpty(txt_fName.Text))
+                {
+                    MessageBox.Show("First Name cannot have numbers or cannot be blank", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else if (txt_lname.Text.Any(char.IsDigit) || string.IsNullOrEmpty(txt_lname.Text))
+                {
+                    MessageBox.Show("Last Name cannot have numbers or cannot be blank", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else if (txt_phoneNo.Text.Any(char.IsLetter) || string.IsNullOrEmpty(txt_phoneNo.Text))
+                {
+                    MessageBox.Show("Telephone cannot be empty or cannot have letters", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else if (string.IsNullOrEmpty(txt_cid.Text))
+                {
+                    MessageBox.Show("Customer ID cannot be blank", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else if (string.IsNullOrEmpty(txt_Address.Text))
+                {
+                    MessageBox.Show("Address cannot be blank", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else if (txt_nicNumber.Text.Length == 0)
+                {
+                    MessageBox.Show("NIC number cannot be blank", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else if (txt_nicNumber.Text.Any(char.IsLetter))
+                {
+                    MessageBox.Show("NIC number cannot have letters", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    MessageBox.Show(this, "data saved succesfully", "information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                con.Close();
+            }
+
+            catch (FormatException)
+            {
+                MessageBox.Show("Enter Numbers only", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("somthing went wrong", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Btn_clear_Click(object sender, EventArgs e)
+        {
+            txt_cid.Clear();
+            txt_fName.Clear();
+            txt_lname.Clear();
+            txt_Email.Clear();
+            txt_Address.Clear();
+            txt_nicNumber.Clear();
+            txt_phoneNo.Clear();
+        }
+    }
+}
